16c2f0c4e2673397d2acd3588a16f92adfe6abbc
Egemen Ceylan <egemen.ceylan1@gmail.com>
2021-12-22 01:26:27 +0300

    updated wordy comp

--- src/components/Wordy.tsx
+++ src/components/Wordy.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { KeyValue } from '../models/key-value.model';
 import { WordList } from '../models/word-list.model';
@@ -9,19 +9,37 @@ function Wordy() {
     let navigate = useNavigate();
     let { userId } = useParams();
 
-    if (!userId) navigate(`/`);
+    //#region Component validation
+    useEffect(() => {
+        if (!userId) navigate(`/`);
+    }, [userId, navigate])
+
+    if (!userId) return null;
+    //#endregion
+
     let wordList: WordList[] = [];
+
+    let getDropdownDatas = () => {
+        let datas: KeyValue[] = [];
+        wordList.forEach(list => datas.push({ key: list.userId, value: list.listName }));
+        return datas;
+    }
+
+    let addWordList = () => {
+        console.log(`add word list`);
+    }
 
     return (
         <Layout title={`Wordy is the best app in the world !`} layoutClasses="wordy-app">
             <Dropdown
                 title={`Select a List !`}
                 defaultOption={`Word Lists`}
-                datas={(() => {
-                    let datas: KeyValue[] = [];
-                    wordList.forEach(list => datas.push({ key: list.userId, value: list.listName }));
-                    return datas;
-                })()}
+                datas={getDropdownDatas()}
+                showCrudIcons={true}
+                disableEDButtons={!wordList || wordList.length < 1}
+                onAddClicked={() => addWordList()}
+                onEditClicked={() => console.log(`Wordy onEditClicked`)}
+                onDeleteClicked={() => console.log(`Wordy onDeleteClicked`)}
                 onSelectionChange={(userId: string) => console.log(`get words with userId: `, userId)}
             />
         </Layout>
